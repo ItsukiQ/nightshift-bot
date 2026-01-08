10ab0e9e9a2db5c91a2610a94fa2ef9f9f0fc035
Max <severo.max03@gmail.com>
2026-01-08 21:00:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 import json
 import os
 
@@ -9,7 +10,6 @@ with open("configuration.json", "r") as config:
 	token = data["token"]
 	prefix = data["prefix"]
 	owner_id = data["owner_id"]
-
 
 class Greetings(commands.Cog):
 	def __init__(self, bot):
@@ -32,5 +32,44 @@ async def on_ready():
 	print(f"We have logged in as {bot.user}")
 	print(discord.__version__)
 	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name =f"{bot.command_prefix}help"))
+		
+
+@bot.tree.command(name="join")
+async def join(interaction: discord.Interaction):
+	if not interaction.user.voice:
+		await interaction.response.send_message(
+			"hala wala ka sa call teh",
+			ephemeral=True
+		)
+		return
+	
+	vc = interaction.user.voice.channel
+	if interaction.guild.voice_client:
+		await interaction.response.send_message(
+			"nasa na ako gurl kalma",
+			ephemeral=True
+		)
+		return
+	
+	# try to join the VC
+	try:
+		voice_client = await vc.connect()
+		print(f"Connected to voice channel: {vc.name}")
+	except Exception as e:
+		print(f"Error joining voice channel: {e}")
+		await interaction.response.send_message(
+			"di ako teh nakajoin sorry",
+			ephemeral=True
+		)
+
+@bot.tree.command(name="leave")
+async def leave(interaction: discord.Interaction):
+	return
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+	if member.id == bot.user.id:
+		if before.channel and not after.channel:
+			print("Nightshift Bot was disconnected from the voice channel")
 
 bot.run(token)
